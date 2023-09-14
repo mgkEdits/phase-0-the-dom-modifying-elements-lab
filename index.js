d1be56fde63937d97d77ef39519effed465014a1
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-14 15:49:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+const newContent = document.createTextNode("Elijah Nyasiando is the champion");
+newHeader.appendChild(newContent);
+document.body.append(newHeader);
+
